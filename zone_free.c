92461e320ea4d968e404011fc6daae729dc8c51c
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2018-08-22 13:21:24 +0300

    fixed error in  free()

--- zone_free.c
+++ zone_free.c
@@ -6,6 +6,9 @@ static int iter_tiny_regions(void *ptr) {
     zone_tiny_t *reg = treg;
     int         ret;
 
+    if (!reg)
+        return FREE_ERR_WRONG_REG;
+
     while (reg) {
         ret = tiny_free(reg, ptr);
         if (ret == FREE_SUCCESS || ret == FREE_ERR_WRONG_ADDR)
@@ -21,6 +24,9 @@ static int iter_small_regions(void *ptr) {
     zone_small_t *reg = sreg;
     int          ret;
 
+    if (!reg)
+        return FREE_ERR_WRONG_REG;
+
     while (reg) {
         ret = small_free(reg, ptr);
         if (ret == FREE_SUCCESS || ret == FREE_ERR_WRONG_ADDR)
@@ -35,6 +41,9 @@ static int iter_large_regions(void *ptr) {
 
     zone_large_t *reg = lreg;
     int          ret;
+
+    if (!reg)
+        return FREE_ERR_WRONG_REG;
 
     while (reg) {
         ret = large_free(reg, ptr);
