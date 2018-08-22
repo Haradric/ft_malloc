e9c489f00331a19ae7ea5e2ea75e04a6000b9a29
Nikita Braslavskiy <nikita.braslavskiy@gmail.com>
2018-08-22 13:54:20 +0300

    added realloc for large regions

--- zone_large_realloc.c
+++ zone_large_realloc.c
@@ -1,13 +1,28 @@
 
+#include <sys/mman.h>
 #include "libft_malloc_zone.h"
 
 void    *large_realloc(zone_large_t *reg, void *ptr, size_t size) {
 
-    (void)reg;
-    (void)ptr;
-    (void)size;
+    void *new;
 
-    debug("%s is not implemented yet\n", __FUNCTION__);
+    // check if the pointer is located in the region
+    if (ptr < (void *)&reg->block && \
+        ptr >= (void *)&reg->block + reg->meta.bytes)
+        return REALLOC_FAILURE;
 
-    return NULL;
+    // check whether the pointer is the begining of the block
+    if (ptr != reg->block)
+        return REALLOC_FAILURE;
+
+    new = mmap(0, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (!new)
+        return REALLOC_FAILURE;
+    libft_memcpy(new, reg->block, reg->meta.bytes);
+    munmap(reg->block, reg->meta.bytes);
+    reg->block = new;
+    reg->meta.bytes = size;
+
+    // debug message
+    return reg->block;
 }
